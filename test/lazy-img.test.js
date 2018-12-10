702f5484ceb1f8aa6dc9e34db68130cb33fa297c
Danny Moerkerke <danny@dannymoerkerke.nl>
2018-12-10 13:47:11 +0100

    updated tests and demo file

--- test/lazy-img.test.js
+++ test/lazy-img.test.js
@@ -4,7 +4,7 @@ describe('lazy-img', () => {
   let element;
 
   beforeEach(() => {
-    element = document.createElement('lazy-img');
+    element = document.createElement('img', {is: 'lazy-img'});
 
     document.body.appendChild(element);
   });
@@ -31,24 +31,6 @@ describe('lazy-img', () => {
     element.removeObserver();
 
     assert.equal(element.observer, null);
-  });
-
-  it('should remove the observer and dispatch a "load" event when the image is loaded', () => {
-    const spy1 = sinon.spy(element, 'removeObserver');
-    const spy2 = sinon.spy(element, 'dispatchEvent');
-
-    const event = new CustomEvent('load', {
-      composed: true,
-      bubbles: true
-    });
-
-    element.onImageLoaded();
-
-    assert.equal(element.removeObserver.called, true);
-    assert.deepEqual(element.dispatchEvent.calledWith(event), true);
-
-    spy1.restore();
-    spy2.restore();
   });
 
   it('should load the image through "setTimeout" when the element is fully visible', (done) => {
@@ -84,39 +66,19 @@ describe('lazy-img', () => {
     const src = 'foo.jpg';
     element.loadImage();
 
-    assert.equal(element.image.getAttribute('src'), null);
+    assert.equal(element.getAttribute('src'), null);
 
-    element.setAttribute('src', src);
+    element.setAttribute('data-src', src);
     element.loadImage();
 
-    assert.equal(element.image.getAttribute('src'), src);
+    assert.equal(element.getAttribute('src'), src);
   });
 
-  it('should set dimensions on the image and container when the "width" and "height" attributes change' , () => {
-    assert.equal(element.image.hasAttribute('width'), false);
-    assert.equal(element.image.hasAttribute('height'), false);
-
-    element.attributeChangedCallback('width', null, 640);
-    element.attributeChangedCallback('height', null, 480);
-
-    assert.equal(element.image.getAttribute('width'), '640');
-    assert.equal(element.image.getAttribute('height'), '480');
-    assert.equal(element.container.style.width, '640px');
-    assert.equal(element.container.style.height, '480px');
-  });
 
   it('should set the loading delay when the "delay" attribute changes', () => {
     assert.equal(element.delay, 500);
 
     element.setAttribute('delay', '750');
     assert.equal(element.delay, 750);
-  });
-
-  it('should remove the "src" attribute from the image to unload it', () => {
-    element.image.setAttribute('src', 'foo.jpg');
-
-    element.unloadImage();
-
-    assert.equal(element.image.hasAttribute('src'), false);
   });
 });
